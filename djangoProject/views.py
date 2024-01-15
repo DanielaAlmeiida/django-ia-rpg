a04eaa860b10325af7f90cdbf60381edf60cee3a
Daniela Almeida <danielaspbrasil@gmail.com>
2024-01-15 20:02:41 -0300

    Mostrar dados da carta criada e ajustes no CSS, responsivo

--- djangoProject/views.py
+++ djangoProject/views.py
@@ -1,5 +1,7 @@
 import base64
 import os
+import random as rd
+
 from dotenv import load_dotenv
 from openai import OpenAI
 
@@ -11,9 +13,9 @@ import requests
 
 def generateImage(request):
     if request.method == 'POST':
-        load_dotenv()
 
-        api_key = os.getenv("SEGMIND_API_KEY")
+        load_dotenv()
+        secret_key = os.getenv("SEGMIND_API_KEY")
         client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
         url = "https://api.segmind.com/v1/sdxl1.0-samaritan-3d"
@@ -33,18 +35,18 @@ def generateImage(request):
                  "content": prompt + "\nAfter creating the weapon description, summarize the description"},
             ],
             model="gpt-3.5-turbo",
-            max_tokens=50
+            max_tokens=45
         )
 
         final_prompt = response_text.choices[0].message.content
 
-        description = final_prompt
         print(final_prompt)
 
         data = {
           "prompt": final_prompt,
           "negative_prompt": "drawing, painting, crayon, sketch, graphite, impressionist, noisy, blurry, soft, "
-                             "deformed, ugly. young. long neck. (cross eyed:1.5). multiple characters, character, person",
+                             "deformed, ugly. young. long neck. (cross eyed:1.5). multiple characters, character, "
+                             "person, Letters, names, words, Human being, doll",
           "samples": 1,
           "scheduler": "Euler a",
           "num_inference_steps": 25,
@@ -55,8 +57,113 @@ def generateImage(request):
           "base64": False
         }
 
-        response = requests.post(url, json=data, headers={'x-api-key': api_key})
+        response = requests.post(url, json=data, headers={'x-api-key': secret_key})
         print(response)
+
+        def status_generetor():
+
+            status = 0
+            lucky = rd.randint(1, 100)
+
+            if lucky <= 40:
+                status = rd.randint(10, 80)
+            elif lucky <= 65:
+                status = rd.randint(81, 130)
+            elif lucky <= 80:
+                status = rd.randint(131, 180)
+            elif lucky <= 90:
+                status = rd.randint(181, 220)
+            elif lucky <= 95:
+                status = rd.randint(221, 260)
+            elif lucky <= 97:
+                status = rd.randint(261, 300)
+            elif lucky <= 99:
+                status = rd.randint(301, 350)
+            else:
+                status = rd.randint(351, 500)
+
+            return status
+
+        def item_status():
+
+            stat = status_generetor()
+
+            if stat <= 80:
+                rarity = "Bronze"
+                stt = stat
+            elif stat <= 130:
+                rarity = "Silver"
+                stt = stat
+            elif stat <= 180:
+                rarity = "Gold"
+                stt = stat
+            elif stat <= 220:
+                rarity = "Emerald"
+                stt = stat
+            elif stat <= 260:
+                rarity = "Diamant"
+                stt = stat
+            elif stat <= 300:
+                rarity = "Ruby"
+                stt = stat
+            elif stat <= 350:
+                rarity = "Obsidian"
+                stt = stat
+            else:
+                rarity = "Opal"
+                stt = stat
+
+            return rarity, stt
+
+        rarity_card, statu = item_status()
+
+        ore_img = ""
+        efects = ["Raio", "Trovão", "Veneno", "Gelo", "Fogo", "Divino", "Mágico", "Sombras",
+                  "Arco-Íris", "Terra", "Plasma", "Tempestade", "Estelar", "Arcano"]
+
+        if rarity_card == "Bronze":
+            ore_img = "../static/css/img-ores/ore-Bronze.png"
+            i = 1
+            for number in range(0, i):
+                print(item_status())
+        elif rarity_card == "Silver":
+            ore_img = "../static/css/img-ores/ore-Silver.png"
+            i = 2
+            for number in range(0, i):
+                print(item_status())
+        elif rarity_card == "Gold":
+            ore_img = "../static/css/img-ores/ore-Gold.png"
+            i = 3
+            for number in range(0, i):
+                print(item_status())
+        elif rarity_card == "Emerald":
+            ore_img = "../static/css/img-ores/ore-Emerald.png"
+            i = 4
+            for number in range(0, i):
+                print(item_status())
+        elif rarity_card == "Diamant":
+            ore_img = "../static/css/img-ores/ore-Diamant.png"
+            i = 5
+            for number in range(0, i):
+                print(item_status())
+        elif rarity_card == "Ruby":
+            ore_img = "../static/css/img-ores/ore-Ruby.png"
+            i = 6
+            for number in range(0, i):
+                print(item_status())
+        elif rarity_card == "Obsidian":
+            ore_img = "../static/css/img-ores/ore-Obsidian.png"
+            i = 6
+            for number in range(0, i):
+                print(item_status())
+            print(efects[rd.randint(0, 14)])
+        else:
+            ore_img = "../static/css/img-ores/ore-Opal.png"
+            i = 6
+            for number in range(0, i):
+                print(item_status())
+            print(efects[rd.randint(0, 14)])
+            print(efects[rd.randint(0, 14)])
 
         if response.status_code == 200:
             img = Image.open(BytesIO(response.content))
@@ -66,15 +173,27 @@ def generateImage(request):
             img.save(byte_stream, format='PNG')
             byte_stream.seek(0)
 
-            imagem_base64 = base64.b64encode(byte_stream.getvalue()).decode('utf-8')
-            x = ''
+            weapon_suf = weapon_type[0] + weapon_type[1]
+            serial_number = "0001"
+            type_weapon = f"[{weapon_type} - {weapon_suf}{serial_number}]"
+            card_name = weapon_name
+            x, y, z = "hidden", "display: none", "display: block"
+            image_base64 = base64.b64encode(byte_stream.getvalue()).decode('utf-8')
+            description = final_prompt
 
             context = {
-                'imagem_base64': imagem_base64,
-                'description': description,
+                'ore_img': ore_img,
+                'rarity_card': rarity_card,
+                'type_weapon': type_weapon,
+                'cardName': card_name,
+                'z': z,
+                'y': y,
                 'x': x,
+                'description': description,
+                'image_base64': image_base64,
             }
 
             return render(request, 'generate_image.html', context)
 
     return render(request, 'generate_image.html')
+
--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,21 @@
-const questionMark = document.querySelector('#question-mark')
+/*const questionMark = document.querySelector('#question-mark')
 const btnForge = document.querySelector('.btn-forge');
-
-console.log("Teste 1");
-
 document.addEventListener('DOMContentLoaded', function () {
-    console.log("Teste 2");
-
     btnForge.addEventListener('click', () => {
         console.log("Teste clique botao");
     });
 });
 
+const btnForge = document.querySelector(".btn-forge, .generated-img");
+btnForge.addEventListener(
+  "beforematch",
+  function () {
+    btnForge.addEventListener('click')
+  });
+
+const btnForge = document.querySelector('.btn-forge');
+untilFound.addEventListener(
+    'beforematch',
+    function () {
+    btnForge.addEventListener('click');
+});*/
